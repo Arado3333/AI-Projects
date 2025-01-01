6ef6c67cc7077342fe1fbdcd2760148093d5b05d
Arad David <arado3333@gmail.com>
2025-01-01 14:22:35 +0200

    ch

--- brainjs/js/index.js
+++ brainjs/js/index.js
@@ -1,24 +1,24 @@
 import { data } from "./data.js";
+import fs from "node:fs/promises";
 
 console.log(brain);
 
-const network = new brain.recurrent.LSTM(
-    {
-        inputSize: 1,
-        outputSize: 1,
-        hiddenLayers: [4,4]
-    }
-);
+let network = new brain.recurrent.LSTM({
+  inputSize: 1,
+  outputSize: 1,
+  hiddenLayers: [4, 8, 8],
+});
 
 network.train(data, {
-    iterations: 13000,
-    errorThresh: 0.05,
-    logPeriod: 50,
+    iterations: 20000,
+    errorThresh: 0.003,
+    logPeriod: 100,
     log: (stats) => console.log(stats)
 });
 
+fs.writeFileSync('trained-net.js', `export default ${ network.toFunction().toString() };`);
 
 const answer = network.run("world war");
 
-document.querySelector('#answer').innerHTML = answer;
-document.querySelector('#app').innerHTML = brain.utilities.toSVG(network);
+document.querySelector("#answer").innerHTML = answer;
+document.querySelector("#app").innerHTML = brain.utilities.toSVG(network);
